8280f8d14233586f20bac1fa212637e0da215969
naoki-funawatari <g011c1180@gmail.com>
2021-01-13 18:40:01 +0900

    tables

---
+++ src/feature/Tables/Table.jsx
@@ -0,0 +1,47 @@
+import React, { useState } from "react";
+import ghostRepository from "../../data/ghostRepository.json";
+import responseRepository from "../../data/responseRepository.json";
+import "./Table.scss";
+const Table = () => {
+  const ghosts = ghostRepository.ghosts;
+  const responses = responseRepository.responses;
+  const initialItems = ghosts.map(ghost => {
+    return {
+      ghostId: ghost.id,
+      ghostName: ghost.name,
+      responsesNames: ghost.responses.map(response => {
+        return {
+          responsesName: responses.find(o => o.id === response).name,
+        };
+      }),
+    };
+  });
+  const [items, setItem] = useState(initialItems);
+
+  return (
+    <table className="ghost-table">
+      <thead>
+        <th>ID</th>
+        <th>名称</th>
+        <th>証拠①</th>
+        <th>証拠②</th>
+        <th>証拠③</th>
+      </thead>
+      <tbody>
+        {items.map(item => {
+          return (
+            <tr>
+              <td>{item.ghostId}</td>
+              <td>{item.ghostName}</td>
+              {item.responsesNames.map(responsesName => {
+                return <td>{responsesName.responsesName}</td>;
+              })}
+            </tr>
+          );
+        })}
+      </tbody>
+    </table>
+  );
+};
+
+export default Table;
